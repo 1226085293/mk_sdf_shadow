0b6c1ed73512a52fbf679b28fc5b80c2215fb6e0
yuanfengxin <1226085293@qq.com>
2022-03-21 09:37:25 +0800

    # ...

--- assets/sdf_shadow.ts
+++ assets/sdf_shadow.ts
@@ -39,4 +39,68 @@ export class sdf_shadow extends Component {
 			this
 		);
 	}
+	// 合并
+merge(d1: number, d2: number) {
+	return Math.min(d1, d2);
+}
+  // 距离场函数
+  scene_dist(render_v2: cc.Vec2) {
+		let dist = 0;
+
+
+	let light_polygon_j_v2 = cc.v2();
+	let end_index_i = 0;
+	let d = 0.0;
+	  let s = 0.0;
+	  let all_polygon_pos_size = 12;
+	  let light_polygon_v4s = [
+		cc.v2(100.0, 100.0),
+		cc.v2(200.0, 100.0),
+		cc.v2(200.0, 150.0),
+		cc.v2(-1.0, -1.0),
+	]
+	// 录入多边形坐标数量列表
+	for (let k_i = 0; k_i < all_polygon_pos_size; ++k_i) {
+		// j = N - 1;
+		if (k_i == end_index_i) {
+			d = cc.Vec2.dot(render_v2.clone().subtract(light_polygon_v4s[k_i]), render_v2.clone().subtract(light_polygon_v4s[k_i]));
+			s = 1.0;
+			for (let k2_i = 0; k2_i < all_polygon_pos_size; ++k2_i) {
+				if (k2_i <= k_i) {
+					continue;
+				}
+				if (light_polygon_v4s[k2_i].x == -1.0) {
+				dist = this.merge(dist, s * Math.sqrt(d));
+				if (end_index_i == 0) {
+					end_index_i = k2_i;
+					light_polygon_j_v2 = light_polygon_v4s[k2_i - 1].clone();
+				} else {
+					end_index_i = k2_i;
+					light_polygon_j_v2 = light_polygon_v4s[k2_i - 1].clone();
+					// 结尾用两个-1
+					if (light_polygon_v4s[k2_i].y == -1.0) {
+					break;
+					}
+					continue;
+				}
+				}
+			}
+		}
+		
+		let e = light_polygon_j_v2.clone().subtract(light_polygon_v4s[k_i]);
+		let w = render_v2.clone().subtract(light_polygon_v4s[k_i]);
+		let b = w.clone().subtract(e) * clamp(dot(w, e) / dot(e, e), 0.0, 1.0);
+		d = min(d, dot(b, b));
+		bvec3 c = bvec3(render_v2.y >= light_polygon_v4s[k_i].y, render_v2.y < light_polygon_j_v2.y, e.x * w.y > e.y * w.x);
+		if(all(c) || all(not(c))) {
+			s *= -1.0;
+		}
+		light_polygon_j_v2 = light_polygon_v4s[k_i].xy;
+	}
+	
+	
+
+	return dist;
+	// return merge(dist, dist2);
+	}
 }
