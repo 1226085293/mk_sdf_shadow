a5ad2550b7454ee9d949a2738c8a9217bb5f2c1f
muzzik <1226085293@qq.com>
2022-03-24 22:59:37 +0800

    # ...

--- assets/sdf_shadow.ts
+++ assets/sdf_shadow.ts
@@ -5,11 +5,23 @@ const { ccclass, property } = _decorator;
 /**用于传递 effect 参数 */
 @ccclass("sdf_shadow")
 export class sdf_shadow extends Component {
+	onLoad() {
+		cc.game.frameRate = 500;
+	}
 	start() {
-		/**canvas 尺寸 */
-		let content_size = cc.view.getCanvasSize();
+		// let _canvas = document.getElementById('capture_canvas');
+		// if (!_canvas) { _canvas = document.createElement('canvas');
+		// const gl = _canvas['getContext']("webgl");
+		// if (!gl) {
+		// 	cc.log("webgl");
+		// }
+		// const gl2 = _canvas['getContext']("webgl2");
+		// if (!gl2) {
+		// 	cc.log("webgl2");
+		// }
 
 		let material = this.node.getComponent(cc.Sprite).material;
+
 		// 圆
 		material.setProperty("light_circle_v4s", [
 			cc.v4(200, 100, 20),
@@ -31,12 +43,12 @@ export class sdf_shadow extends Component {
 			cc.v2(-1),
 		]);
 		// 多边形
-		material.setProperty("light_polygon_v4s", [
-			cc.v2(600, 100),
-			cc.v2(700, 100),
-			cc.v2(700, 200),
-			cc.v2(-1),
-		]);
+		// material.setProperty("light_polygon_v4s", [
+		// 	cc.v2(600, 100),
+		// 	cc.v2(700, 100),
+		// 	cc.v2(700, 200),
+		// 	cc.v2(-1),
+		// ]);
 
 		// 光坐标 & 半径
 		material.setProperty("light_pos_radius_v4s", [cc.v4(200, 100, 300), cc.v2(-1)]);
@@ -52,12 +64,12 @@ export class sdf_shadow extends Component {
 		//
 
 		let temp_v2 = cc.v2();
-		let temp_v4 = cc.v4(0, 0, 300);
+		let temp_v4 = cc.v4(0, 0, 500);
 		let light_pos_v2s_handle = material.passes[0].getHandle("light_pos_radius_v4s");
 		this.node.on(
 			cc.Node.EventType.TOUCH_MOVE,
 			(event_: cc.EventTouch) => {
-				event_.getUILocation(temp_v2);
+				event_.getLocation(temp_v2);
 				temp_v4.x = temp_v2.x;
 				temp_v4.y = temp_v2.y;
 				material.passes[0].setUniformArray(light_pos_v2s_handle, [
